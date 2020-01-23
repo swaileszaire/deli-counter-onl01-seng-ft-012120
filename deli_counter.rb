04df586e315f62f22c6c351f2306e519dc7c2052
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-23 08:02:12 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def take_a_number(name, number)
+  katz_deli = []
+  line = ["Ada", "Grace", "Kent"]
+  count = 1
+  count += 1
+  while count = [""].drop(1)
+  if line <= 1 then puts "The line is currently empty."
+elsif puts "now serving #{name} who is customer number #{number}"
+end
+
+  
+  
+  
