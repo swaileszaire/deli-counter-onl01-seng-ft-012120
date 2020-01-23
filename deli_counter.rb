1cedc600a8614dfb8edd7362d453a89fc5f7e98f
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-23 07:59:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,10 @@ def take_a_number(name, number)
   line = ["Ada", "Grace", "Kent"]
   count = 1
   count += 1
-  while count = [""].
+  while count = [""].drop(1)
   if line <= 1 then puts "The line is currently empty."
 elsif puts "now serving #{name} who is customer number #{number}"
+end
 
   
   
